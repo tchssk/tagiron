a69fccb900e506a9724cda2720d3592d1cc66655
Taichi Sasaki <tchsskk@gmail.com>
2019-03-25 00:25:05 +0900

    Implement Tiles.Shuffle()

--- tile.go
+++ tile.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 type Tile struct {
@@ -13,4 +14,13 @@ type Tiles []Tile
 
 func (t Tile) String() string {
 	return fmt.Sprintf("%s%d", t.Color.String(), t.Number)
+}
+
+func (t *Tiles) Shuffle() {
+	tt := *t
+	for i := range tt {
+		j := rand.Intn(i + 1)
+		tt[i], tt[j] = tt[j], tt[i]
+	}
+	t = &tt
 }
