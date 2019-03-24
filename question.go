dfe9bfca122d7a3e9e93a89362af91e8890a85af
Taichi Sasaki <tchsskk@gmail.com>
2019-03-24 20:47:22 +0900

    Add actual questions

--- question.go
+++ question.go
@@ -1,3 +1,27 @@
 package main
 
 type Questions []string
+
+const (
+	QuestionSumOfAll         = "数字タイルすべての数の合計は？"
+	QuestionSumOfLowerThree  = "小さいほうから３枚の数の合計は？"
+	QuestionSumOfCenterThree = "中央の３枚の数の合計は？"
+	QuestionSumOfUpperThree  = "大きいほうから３枚の数の合計は？"
+	QuestionSumOfRed         = "赤の数の合計は？"
+	QuestionSumOfBlue        = "青の数の合計は？"
+	QuestionDifference       = "数字タイルの最大の数から、最小の数を引いた数は？"
+	QuestionOdd              = "奇数は何枚ある？"
+	QuestionEven             = "偶数は何枚ある？（０も含む）"
+	QuestionNumberPairs      = "同じ数字タイルのペアは何組ある？"
+	QuestionColorPairs       = "同じ色がとなり合っている数字タイルはどこ？"
+	QuestionCenter           = "中央の数字タイルは５以上？４以下？"
+	QuestionSerial           = "数が連続している数字タイルはどこ？"
+	QuestionRedTiles         = "赤の数字タイルは何枚ある？"
+	QuestionBlueTiles        = "青の数字タイルは何枚ある？"
+	QuestionZero             = "０はどこ？"
+	QuestionOneOrTwo         = "１または２はどこ？（どちらかひとつ選ぶ）"
+	QuestionThreeOrFour      = "３または４はどこ？（どちらかひとつ選ぶ）"
+	QuestionFive             = "５はどこ？"
+	QuestionSixOrSeven       = "６または７はどこ？（どちらかひとつ選ぶ）"
+	QuestionEightOrNine      = "８または９はどこ？（どちらかひとつ選ぶ）"
+)
