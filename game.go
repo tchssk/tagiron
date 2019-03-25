2248b367d44824c2bf16680fb14896a746e89ca2
Taichi Sasaki <tchsskk@gmail.com>
2019-03-26 07:56:12 +0900

    Add a constructor for Game

--- game.go
+++ game.go
@@ -2,6 +2,22 @@ package main
 
 type Game struct {
 	Tiles     Tiles
+	Stock     Questions
 	Questions Questions
 	Players   Players
+}
+
+func NewGame() Game {
+	var (
+		tiles = NewTiles()
+		stock = NewQuestions()
+	)
+	tiles.Shuffle()
+	stock.Shuffle()
+	return Game{
+		Tiles:     tiles,
+		Stock:     stock,
+		Questions: Questions{},
+		Players:   Players{},
+	}
 }
