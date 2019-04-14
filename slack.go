100b93be32579f8d9d894c7ef8ec43a586e794f3
Taichi Sasaki <tchsskk@gmail.com>
2019-04-14 23:59:59 +0900

    Add PostMessage()

--- slack.go
+++ slack.go
@@ -8,3 +8,12 @@ type SlackListener struct {
 	client *slack.Client
 	botID  string
 }
+
+func PostMessage(c *slack.Client, channelID, message string) error {
+	_, _, err := c.PostMessage(
+		channelID,
+		slack.MsgOptionText(message, false),
+		slack.MsgOptionPostMessageParameters(slack.NewPostMessageParameters()),
+	)
+	return err
+}
