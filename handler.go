59772cc01a846a3bbdefb28ba2c788640555d752
Taichi Sasaki <tchsskk@gmail.com>
2019-04-19 23:59:59 +0900

    Add SlackListener.handleJoin()

--- handler.go
+++ handler.go
@@ -18,3 +18,22 @@ func (s *SlackListener) handleNew(ev *slack.MessageEvent, m []string) error {
 	}
 	return nil
 }
+
+func (s *SlackListener) handleJoin(ev *slack.MessageEvent, m []string) error {
+	user, err := s.client.GetUserInfo(ev.User)
+	if err != nil {
+		return err
+	}
+	player := NewPlayer(user.Profile.RealName, user.ID)
+	player.Tiles.Add(game.Tiles.Remove(0, 5))
+	player.Tiles.Sort()
+	game.Players.Add(player)
+
+	if err := PostMessage(s.client, ev.Channel, fmt.Sprintf("%s さんが参加しました。", user.Profile.RealName)); err != nil {
+		return fmt.Errorf("failed to post message: %v", err)
+	}
+	if err := PostEphemeral(s.client, ev.Channel, ev.User, fmt.Sprintf("あなたのタイル:\n%v", player.Tiles)); err != nil {
+		return fmt.Errorf("failed to post message: %v", err)
+	}
+	return nil
+}
