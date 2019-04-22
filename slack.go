e937473c12a68844609b2dddb982c5aa3942d1c8
Taichi Sasaki <tchsskk@gmail.com>
2019-04-22 23:59:59 +0900

    Add help()

--- slack.go
+++ slack.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"text/tabwriter"
 
 	"github.com/nlopes/slack"
+)
+
+const (
+	commandPing     = "ping"
+	commandHelp     = "help"
+	commandNew      = "new"
+	commandJoin     = "join"
+	commandQuestion = "question"
 )
 
 type SlackListener struct {
@@ -32,4 +42,24 @@ func PostEphemeral(c *slack.Client, channelID, userID, message string) error {
 
 func unknown(command string) string {
 	return fmt.Sprintf("Unknown command \"%s\".", command)
+}
+
+func help() string {
+	var buf bytes.Buffer
+	w := tabwriter.NewWriter(&buf, 0, 0, 2, ' ', 0)
+	for _, command := range []struct {
+		name        string
+		description string
+	}{
+		{commandPing, ""},
+		{commandHelp, ""},
+		{commandNew, "新しいゲームを始める"},
+		{commandJoin, "ゲームに参加する"},
+		{commandQuestion, "場の質問を一覧する"},
+		{fmt.Sprintf("%s <number>", commandQuestion), "質問する"},
+	} {
+		fmt.Fprintf(w, "%s\t%s\n", command.name, command.description)
+	}
+	w.Flush()
+	return "```" + buf.String() + "```"
 }
